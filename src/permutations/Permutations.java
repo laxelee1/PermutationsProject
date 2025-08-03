ed0c19eae57f64d6496d1206b40a967989910cac
Alex Lee <laxelee1@gmail.com>
2025-08-03 19:36:17 +0100

    Imported List and added some comments

--- src/permutations/Permutations.java
+++ src/permutations/Permutations.java
@@ -1,12 +1,16 @@
 package permutations;
 
+import java.util.List;
 import java.util.ArrayList;
 
 public class Permutations {
 	private String initWord;
+	private List<String> words;
 
 	public Permutations(String initWord) {
 		this.initWord = initWord;
+		this.words = new ArrayList<String>();
+		// added the word array list
 	}
 
 	public String toString() {
