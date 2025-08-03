c511150bacf0287f230e5f9a6ea7c2b1d3e4ddf2
Alex Lee <laxelee1@gmail.com>
2025-08-03 22:44:40 +0100

    Added some of the basics to the dfs helper function

--- src/permutations/Permutations.java
+++ src/permutations/Permutations.java
@@ -6,21 +6,25 @@ import java.util.ArrayList;
 public class Permutations {
 	private String initWord;
 	private List<String> words;
+	private int n;
 
 	public Permutations(String initWord) {
+		this.n = initWord.length();
 		this.initWord = initWord;
-		this.words = new ArrayList<String>();
+		this.words = new ArrayList<>();
 		// added the word array list
-		dfs(6);
+		boolean[] used = new boolean[n];
+		dfs(used);
 	}
 
 	public String toString() {
 		return this.initWord;
 	}
 
-	private void dfs(int i) {
+	private void dfs(boolean[] used) {
 		// recursive helper function to create the permutations
-		System.out.printf("int i is %d%n",i);
-
+		for(int i = 0; i < used.length; i++) {
+			System.out.printf("index %d of used = %b%n", i, used[i]);
+		}
 	}
 }
