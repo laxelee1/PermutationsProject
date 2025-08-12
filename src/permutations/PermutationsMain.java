f3ddeb98eb4e19f1b654e3d3918460c4548968c7
Alex Lee <laxelee1@gmail.com>
2025-08-12 10:59:59 +0100

    Updated PermutationsMain to allow user inputted words

--- src/permutations/PermutationsMain.java
+++ src/permutations/PermutationsMain.java
@@ -1,8 +1,12 @@
 package permutations;
 
+import java.util.Scanner;
+
 public class PermutationsMain {
 	public static void main(String[] args) {
-		String initWord = "walk";
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("What word would you like to get the permutations for? ");
+		String initWord = scanner.nextLine();
 		Permutations permutations = new Permutations(initWord);
 		System.out.println(permutations);
 	}
